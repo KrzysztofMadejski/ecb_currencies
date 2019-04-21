23cd28b7fee1c0183ccea4c90243241477cfcc6c
Krzysztof Madejski <krzysztof@madejscy.pl>
2019-04-21 21:31:20 +0200

    Create exchange_rate model

--- currencies/models.py
+++ currencies/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.core import validators
 
-# Create your models here.
+
+class ExchangeRate(models.Model):
+    """Exchange rate at a given point in time"""
+
+    target_currency = models.CharField(max_length=3, validators=[validators.MinLengthValidator(3)])
+    base_currency = models.CharField(max_length=3, validators=[validators.MinLengthValidator(3)])
+    rate = models.DecimalField(decimal_places=4, max_digits=4+10)
+    at = models.DateTimeField()
+    """Time point which the rate is valid for"""
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['target_currency', 'base_currency', 'at'], name='exchange_rate_unique')
+        ]
+        indexes = [
+            models.Index(fields=['target_currency', 'base_currency'], name='exchange_rate_currencies')  # to query fast given exchange rate history
+        ]
